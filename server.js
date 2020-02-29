b83cc95fd3c42bc9b407bafe80f99487d85bddec
E <evcarone@gmail.com>
2020-02-29 14:02:17 -0800

    server config for passport

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const passport = require('passport');
+// const config = require('./server/config');
+const logger = require('morgan')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +11,50 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+// connect to the database and load models
+// uses environmental variable for deployment (Heroku) or defaults to local config
+const uri = process.env.MONGODB_URI  //|| config.dbUri;
+
+mongoose.connect(uri);
+// plug in the promise library:
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on('error', (err) => {
+	console.error(`Mongoose connection error: ${err}`);
+	process.exit(1);
+});
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// pass the passport middleware
+app.use(passport.initialize());
+
+// load passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
+// routes
+const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
 
 // Send every request to the React app
 // Define any API routes before this runs
