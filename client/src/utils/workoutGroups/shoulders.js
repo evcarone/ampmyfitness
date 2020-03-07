ecca357d1bfbe30abfb71d7ba14df1887821daf5
Zachary Nylen <znylen@aol.com>
2020-03-07 12:33:00 -0800

    Add New Workout Catergories

---
+++ client/src/utils/workoutGroups/shoulders.js
@@ -0,0 +1,44 @@
+const shoulders = [
+    {
+        name: "Lateral Raises",
+        equiptment: "Dumbbell",
+        description: "Hold a dumbbell in each hand at your sides like suitcases. Rotate your palms so that one thumb is pointed out to the side while the other faces forward. Raise the dumbbells to shoulder height. One arm should lift out to the side while the other is raised forward. Return the weights to the starting position.",
+        img_start: "https://wger.de/media/exercise-images/148/lateral-dumbbell-raises-large-2.png",
+        img_end: "https://wger.de/media/exercise-images/148/lateral-dumbbell-raises-large-1.png",
+        muscles: "Anterior deltoid",
+    },
+    {
+        name: "Barbell Shoulder Press",
+        equiptment: "Barbell",
+        description: "Sit on a bench, the back rest should be almost vertical. Take a barbell with a shoulder wide grip and bring it up to chest height. Press the weight up, but don't stretch the arms completely. Go slowly down and repeat.",
+        img_start: "https://wger.de/media/exercise-images/119/seated-barbell-shoulder-press-large-1.png",
+        img_end: "https://wger.de/media/exercise-images/119/seated-barbell-shoulder-press-large-2.png",
+        muscles: "Anterior deltoid",
+    },
+    {
+        name: "Dumbbell Shoulder Press",
+        equiptment: "Dumbbell",
+        description: "Sit on a bench, the back rest should be almost vertical. Take two dumbbells and bring them up to shoulder height, the palms and the elbows point during the whole exercise to the front. Press the weights up, at the highest point they come very near but don't touch. Go slowly down and repeat.",
+        img_start: "https://wger.de/media/exercise-images/123/dumbbell-shoulder-press-large-1.png",
+        img_end: "https://wger.de/media/exercise-images/123/dumbbell-shoulder-press-large-2.png",
+        muscles: "Anterior deltoid",
+    },
+    {
+        name: "Barbell Shrugs",
+        equiptment: "Barbell",
+        description: "Take a barbell and stand with a straight body, the arms are hanging freely in front of you. Lift from this position the shoulders as high as you can, but don't bend the arms during the movement. On the highest point, make a short pause of 1 or 2 seconds before returning slowly to the initial position. When training with a higher weight, make sure that you still do the whole movement!",
+        img_start: "https://wger.de/media/exercise-images/150/Barbell-shrugs-1.png",
+        img_end: "https://wger.de/media/exercise-images/150/Barbell-shrugs-2.png",
+        muscles: "Anterior deltoid",
+    },
+    {
+        name: "Dumbbell Shrugs",
+        equiptment: "Dumbbell",
+        description: "Stand with straight body, the hands are hanging freely on the side and hold each a dumbbell. Lift from this position the shoulders as high as you can, but don't bend the arms during the movement. On the highest point, make a short pause of 1 or 2 seconds before returning slowly to the initial position. When training with a higher weight, make sure that you still do the whole movement!",
+        img_start: "https://wger.de/media/exercise-images/151/Dumbbell-shrugs-2.png",
+        img_end: "https://wger.de/media/exercise-images/151/Dumbbell-shrugs-1.png",
+        muscles: "Anterior deltoid",
+    },
+]
+
+export default shoulders;
