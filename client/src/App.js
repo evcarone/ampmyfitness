751f113cf5c77274f33e73fe23045a3c003e8bd3
Nicholas Serra <nserra310@yahoo.com>
2020-03-10 20:53:05 -0700

    adding files to nick2 branch in order to switch to my origional branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
 import React, { Component } from "react";
+import RandomQuote from "./components/RandomQuotes/RandomQuote";
 // import injectTapEventPlugin from 'react-tap-event-plugin';
-
 import { BrowserRouter as Router,
 Switch,
 Route,
 Link} from "react-router-dom";
-
 import HomePage from './components/HomePage.jsx';
 import { 
   PrivateRoute, 
   PropsRoute, 
   LoggedOutRoute 
 } from './components/Routes';
-
 import LoginPage from './pages/LoginPage.jsx';
 import LogoutFunction from './pages/LogoutFunction.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
-
 import Auth from './utils/Auth';
-
-
 import ButtonAppBar from "./components/ButtonAppBar";
 import MaterialVideo from "./pages/MaterialVideos";
 import Workouts from "./pages/Workouts";
+
 
 // remove tap delay, essential for MaterialUI to work properly
 // injectTapEventPlugin();
@@ -46,20 +42,22 @@ class App extends Component {
 
 
 render() {
+
     return (
       <Router>
         
         <div>
         <ButtonAppBar />
         
-        <br></br>
         <div style={{ height: 'calc(100vh - 64px)', backgroundImage: `url(/images/Background_Image.jpg)`, backgroundSize: 'cover', }}>
+          
+        <br></br>
+          
           <Switch>
             <Route exact path="/materialvideo" component={MaterialVideo} />
             <Route path="/workouts" component={Workouts} />
           </Switch>
-        
-      
+         
       <div>
 
         <PropsRoute exact path="/" component={HomePage} toggleAuthenticateStatus={this.toggleAuthenticateStatus} />
@@ -67,11 +65,21 @@ render() {
         <LoggedOutRoute path="/login" component={LoginPage} toggleAuthenticateStatus={this.toggleAuthenticateStatus} />
         <LoggedOutRoute path="/signup" component={SignUpPage}/>
         <Route path="/logout" component={LogoutFunction}/>
+       
       </div>
-      
-      
+
+      <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '250px', marginRight: '20px', marginLeft: '20px'}}>
+      <container>
+          <RandomQuote />
+        </container>
       </div>
+
       </div>
+
+      </div>
+     
+
+
       
     </Router>
       
